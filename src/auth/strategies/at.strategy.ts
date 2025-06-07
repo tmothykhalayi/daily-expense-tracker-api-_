c92af01026a48575b2745f881732546b82173ea9
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-06-07 20:21:20 +0300

    fix(auth): correct ConfigService reference and improve comments in AtStrategy

--- src/auth/strategies/at.strategy.ts
+++ src/auth/strategies/at.strategy.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
-
 /*
 - Purpose: Validates short-lived access tokens
 - How it works: Extracts the JWT from the Authorization header, verifies it with the secret key, and attaches the payload to the request
@@ -15,15 +14,14 @@ type JWTPayload = {
 
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt-at') {
-  constructor(private readonly configService: ConfigService) {
+  constructor(private readonly configServices: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: configService.get<string>('JWT_ACCESS_TOKEN_SECRET') ?? 'defaultAccessSecret',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), //Bearer token extraction from Authorization header
+      secretOrKey: configServices.getOrThrow<string>('JWT_ACCESS_TOKEN_SECRET'), //Access token secret key
     });
   }
 
-  
   validate(payload: JWTPayload) {
-    return payload;
+    return payload; // Return the payload directly, which contains user information (attach request.user = payload;)
   }
 }
